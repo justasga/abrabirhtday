fe66d972f012745163fd4cc539cd1cdb669e1e66
justasga <74532971+justasga@users.noreply.github.com>
2021-06-07 19:46:01 +0300

    s

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import './style.css';
 
 import * as THREE from  'https://unpkg.com/three/build/three.module.js' 
 
- import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+ // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Setup
 
